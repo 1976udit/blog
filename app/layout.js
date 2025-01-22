dafc4664534ed197f8ecc5fb50bc47c03957b0e5
1975udit <udit2115418@akgec.ac.in>
2025-01-22 13:52:51 +0530

    navbar

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/ui/navbar";
+import ThemeProvider from "@/components/ui/theme-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,10 +22,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
+       <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          ></ThemeProvider>
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       > 
       <Navbar />
         {children}
+      </ThemeProvider>
       </body>
     </html>
   );
--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,32 +1,60 @@
 // components/Navbar.jsx
 "use client"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
-
 import Link from 'next/link';
-import { useState } from 'react';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
-    <nav className="bg-gray-400 text-white p-4 flex justify-between items-center backdrop-blur sticky top-0 bg-background-50">
-      <Link href='/'><div className="text-lg font-bold">BlogIt</div></Link>
-      <div className={`flex flex-col md:flex-row md:items-center md:space-x-4 ${isOpen ? 'block' : 'hidden'} md:block`}>
-        <Link href="/" className="hover:text-gray-300">Home</Link>
-        <Link href="/about" className="hover:text-gray-300">About</Link>
-        <Link href="/contact" className="hover:text-gray-300">Contact</Link>
-      </div>
-      <button className="md:hidden text-xl" onClick={toggleMenu}>
-        ☰
-      </button>
-      <Button variant="outline" className="text-gray-400">Button</Button>
+    <nav className="bg-gray-800 p-4 flex justify-between sticky top-0 bg-background/50 border-b backdrop-blur">
+      <div className="container mx-auto flex justify-between items-center">
+      <Link href='/'> <div className="text-lg font-bold">BlogIt</div> </Link>
+        <div className="hidden md:flex space-x-4 items-center">
+        <Link href="/" className="hover:text-gray-400">Home</Link>
+        <Link href="/about" className="hover:text-gray-400">About</Link>
+        <Link href="/blog" className="hover:text-gray-400">Blog</Link>
+        <Link href="/contact" className="hover:text-gray-400">Contact</Link>
+        <div>
+          <Button className="mx-1 text-xs">Sign Up</Button>
+          <Button className="mx-1 text-xs">Login</Button>
+        </div>
+        </div>
 
+      <div className="md:hidden">
+      <Sheet>
+        <SheetTrigger>
+            <div className=" text-xl" > ☰ </div>
+        </SheetTrigger>
+        <SheetContent className="w-64">
+            <SheetHeader className="flex items-center justify-between">
+            <SheetTitle className="font-bold my-4">BlogIt</SheetTitle>
+            <SheetDescription>
+              <div className="flex flex-col gap-6 justify-center items-center">
+                <Link href="/" className="hover:text-gray-400">Home</Link>
+                <Link href="/about" className="hover:text-gray-400">About</Link>
+                <Link href="/blog" className="hover:text-gray-400">Blog</Link>
+                <Link href="/contact" className="hover:text-gray-400">Contact</Link>
+                <div>
+                    <Button className="mx-1 text-xs">Sign Up</Button>
+                    <Button className="mx-1 text-xs">Login</Button>
+                </div>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+      </div>
+      </div>
     </nav>
-  );
-};
+  )
+  };
 
 export default Navbar;
